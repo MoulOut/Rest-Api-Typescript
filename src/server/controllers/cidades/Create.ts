a38e9fc7a6b66ac3287ae021fc48349ef9202ab4
MoulOut <mmoulinlima@outlook.com>
2023-10-03 06:43:32 -0300

    feat: added yup and interface validators

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,12 +1,30 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
 interface Cidade {
   nome: string,
 }
 
-export const create = (req: Request<{}, {}, Cidade>, res: Response) => {
-  console.log(req.body);
-  
+const bodyValidator: yup.ObjectSchema<Cidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, Cidade>, res: Response) => {
+  let validatedData: Cidade | undefined = undefined;
+
+  try {
+    validatedData = await bodyValidator.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+    return res.json({
+      errors: {
+        default: yupError.message,
+      }
+    });
+  }
+
+  console.log(validatedData);
+
   return res.status(StatusCodes.OK).send('City created.');
 };
